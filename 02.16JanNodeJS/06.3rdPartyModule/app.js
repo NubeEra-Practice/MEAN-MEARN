a0be82a06c9f869914dbcdd3ecbd7e55593721fa
mujahed85 <mujahed.mca@gmail.com>
2023-01-16 20:42:12 +0530

    MongoDB Docker Container MySQL Connectivity

---
+++ 02.16JanNodeJS/06.3rdPartyModule/app.js
@@ -0,0 +1,18 @@
+//npm install assert
+console.clear()
+const assert = require('assert');
+  
+let x = 4;
+let y = 5;
+  
+try {
+  
+    // Checking condition
+    assert(x == y);
+}
+catch {
+  
+    // Error output
+    console.log(
+        `${x} is not equal to ${y}`);
+}
---
+++ 02.16JanNodeJS/07.DatabaseOperations/ConnectDemo.js
@@ -0,0 +1,13 @@
+const { connect } = require("http2");
+var mysql = require("mysql");
+
+var con =mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: ""
+});
+
+con.connect( function(err){
+    if(err) throw err;
+    console.log("Connected");
+});
